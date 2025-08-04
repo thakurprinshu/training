cb349d9fefa2c112d30b3be30b88ce36c2a75b86
Priyanshu Thakur <thakurprinshu1349@gmail.com>
2025-08-04 13:05:01 +0530

    Added airport boarding gate simulation code

---
+++ BoardingGateCheck.java
@@ -0,0 +1,16 @@
+public class BoardingGateCheck {
+    public static void main(String[] args) {
+        int[] actualOrder = {101, 103, 102, 104};
+        int[] expectedOrder = actualOrder.clone();
+        Arrays.sort(expectedOrder);
+
+        int mismatchCount = 0;
+        for (int i = 0; i < actualOrder.length; i++) {
+            if (actualOrder[i] != expectedOrder[i]) {
+                mismatchCount++;
+            }
+        }
+
+        System.out.println("Number of passengers out of order: " + mismatchCount);
+    }
+}
